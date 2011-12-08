5cd719eef41303fc3823528ec3c13cb92e68bc2c
Alan Kennedy <alankennedy100@gmail.com>
2011-12-08 17:43:53 +0000

    Revert "Dont match assets requests with angry mexican"
    This reverts commit 4177a9f13e16b3cb2ab43028e12c7bd7f403b5e7.

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
     end
   end unless ComfortableMexicanSofa.config.admin_route_prefix.blank?
   
-  scope :controller => :cms_content, constraints: { url: /\/^(assets)\//i } do
+  scope :controller => :cms_content do
     get 'cms-css/:site_id/:layout_slug' => :render_css,  :as => 'cms_css'
     get 'cms-js/:site_id/:layout_slug'  => :render_js,   :as => 'cms_js'
     get '/' => :render_html,  :as => 'cms_html',  :path => "(*cms_path)"
